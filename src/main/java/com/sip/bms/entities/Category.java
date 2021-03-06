bfe078a728251cb168862de7e75128ab5e1db730
marouen methnani <m.methnani1@gmail.com>
2021-03-06 23:39:45 +0100

    BMSV1 updated mycategory

--- src/main/java/com/sip/bms/entities/Category.java
+++ src/main/java/com/sip/bms/entities/Category.java
@@ -8,9 +8,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
 @Entity
+@Table (name = "mycategory")
 public class Category {
 
 	@Override
